ea4648caae0b821be383948db69a482466d9a303
Singge Dai <60216386+burlope@users.noreply.github.com>
2023-09-29 02:14:02 +0530

    start start of the project :))

---
+++ RLTWapp/RLTWapp/RLTWappApp.swift
@@ -0,0 +1,17 @@
+//
+//  RLTWappApp.swift
+//  RLTWapp
+//
+//  Created by Kadam Dai on 29/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RLTWappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
