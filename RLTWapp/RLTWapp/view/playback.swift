77926338186f5714d0152c627d65c8c125069780
Singge Dai <60216386+burlope@users.noreply.github.com>
2023-09-30 00:53:20 +0530

    Playback all done(No working buttons yet)

--- RLTWapp/RLTWapp/view/playback.swift
+++ RLTWapp/RLTWapp/view/playback.swift
@@ -12,38 +12,66 @@ struct CustomColor {
 }
 
 struct playback: View {
-    
     var body: some View {
-        
-        ZStack {
-           
-            RoundedRectangle(cornerRadius: 15).fill(CustomColor.lead).frame(width: 393, height: 350).offset(x: 0,y: 350)
-                            
-            ProgressView(value: 0.5).tint(.white).background(.gray).frame(width: 340).offset(y: 310)
+        VStack {
+            Spacer()
 
-                SongImage(image: Image("grad")).offset(x: -140, y: 240)
-            VStack(alignment: .leading){
-                Text("Flashing lights").font(.title2).offset(y:225)
-                Text("Kanye West").offset(y:230)
+            VStack {
+                HStack {
+                    SongImage(image: Image("grad"))
+
+                    VStack(alignment: .leading){
+                        Text("Flashing lights")
+                            .font(.title2)
+                        Text("Kanye West")
+                    }
+                    .padding(.leading, 20)
+
+                    Spacer()
+                }
+                .padding()
+
+                ProgressView(value: 0.5)
+                    .tint(.white)
+                    .background(.gray)
+                    .padding(.horizontal)
+
+                HStack {
+                    Text("1:24")
+                        .foregroundColor(.gray)
+                        .shadow(color: .black, radius: 3)
+                        .font(.system(size: 15))
+                    Spacer()
+                    Text("2:30")
+                        .foregroundColor(.gray)
+                        .shadow(color: .black, radius: 3)
+                        .font(.system(size: 15))
+                }
+                .padding(.horizontal)
+
+                HStack(spacing: 40) {
+                    Image(systemName: "backward.fill")
+                        .font(.system(size: 40))
+                    Image(systemName: "pause.fill")
+                        .tint(.gray)
+                        .font(.system(size: 50))
+                    Image(systemName: "forward.fill")
+                        .font(.system(size: 40))
+                }
+                .padding(.bottom, 20)
             }
-            VStack{
-                Text("1:24").foregroundColor(.gray).shadow(color: .black, radius: 3).font(.system(size: 15)).offset(x:-152,y:335)
-                Text("2:30").foregroundColor(.gray).shadow(color: .black, radius: 3).font(.system(size: 15)).offset(x:150,y:315)
-            }
-            Image(systemName: "pause.fill").tint(.gray)
-                    .font(.system(size: 50)).offset(x:0,y:360)
-                Image(systemName: "forward.fill").font(.system(size: 40)).offset(x:100,y: 360)
-                Image(systemName: "backward.fill").font(.system(size: 40)).offset(x:-100,y: 360)
-                            
-            
-                    
-            
+            .background(
+                CustomColor.lead
+                    .cornerRadius(10)
+                    .ignoresSafeArea()
+                    .overlay(alignment: .bottom) {
+                        CustomColor.lead
+                            .frame(height: 10)
+                    }
+            )
         }
-        
-        }
-        
     }
-
+}
 
 struct playback_Previews: PreviewProvider {
     static var previews: some View {
